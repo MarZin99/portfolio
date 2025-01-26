f3976ad7512bcbbd2ee81b6fb01f1ebf31a4b84e
Marcin Zawadzki <mar.zawadzki99@gmail.com>
2025-01-26 16:05:40 +0100

    Setup prettier, add navi with 2 pages

--- app/_layout/navbar/Navbar.tsx
+++ app/_layout/navbar/Navbar.tsx
@@ -1,9 +1,17 @@
-import styles from "./Navbar.module.css"
+import styles from './Navbar.module.css';
+import Link from 'next/link';
 
 const Navbar = () => {
-    return (
-        <div className={styles.navbar}>NavBar</div>
-    )
-}
+  return (
+    <div className={styles.navbar}>
+      <Link href='/home' className={styles.link}>
+        Home
+      </Link>
+      <Link href='/about' className={styles.link}>
+        About
+      </Link>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,7 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "@/app/_styles/_globals.scss";
 import Navbar from "@/app/_layout/navbar/Navbar";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
-export const metadata: Metadata = {
-  title: "Portfolio",
-  description: "Marcin Zawadzki Portfolio",
-};
 
 export default function RootLayout({
   children,
@@ -25,7 +10,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body >
         <Navbar />
         {children}
       </body>
